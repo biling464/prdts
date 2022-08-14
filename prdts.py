066c992a408fbdba7ece41204dd83c2b0f20eab7
Biling Xu <akaimder@gmail.com>
2022-08-14 18:23:23 +0800

    add check file exist

--- prdts.py
+++ prdts.py
@@ -1,12 +1,19 @@
+import os
 prdts = []
-with open ('prdts.csv', 'r', encoding='utf-8') as f :
-	for line in f :
-		if '商品，價格' in line:
-			continue
+if os.path.isfile('prdts.csv'):
+	print('yea! find')
+	with open ('prdts.csv', 'r', encoding='utf-8') as f :
+		for line in f :
+			if '商品，價格' in line:
+				continue
 		n, p = line.strip().split(',')
 		prdts.append([n, p])
+	print(prdts)
+else:
+	print('no find')
 
-print(prdts)
+
+
 #讓使用者輸入
 while True:
 	n = input("請輸入商品名稱 :")
@@ -16,9 +23,11 @@ while True:
 	p = int(p)
 	prdts.append([n,p])
 print(prdts)
+
 #印出所有購買紀錄
 for p in prdts :
 	print(p[0], "的商品價格為", p[1])
+
 #寫入檔案
 with open ("prdts.csv","w",encoding="utf-8") as f :
 	f.write("商品, 價格\n")
