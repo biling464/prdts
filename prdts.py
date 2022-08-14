e71837c05f9dc1125aa79630d8b629b6f0d15fec
Biling Xu <akaimder@gmail.com>
2022-08-14 17:27:17 +0800

    add read file

--- prdts.py
+++ prdts.py
@@ -1,19 +1,25 @@
 prdts = []
+with open ('prdts.csv', 'r', encoding='utf-8') as f :
+	for line in f :
+		if '商品，價格' in line:
+			continue
+		n, p = line.strip().split(',')
+		prdts.append([n, p])
+
+print(prdts)
+#讓使用者輸入
 while True:
 	n = input("請輸入商品名稱 :")
 	if n == 'q' :
 			break
 	p = input("請輸入商品價格 :")
+	p = int(p)
 	prdts.append([n,p])
 print(prdts)
-
+#印出所有購買紀錄
 for p in prdts :
-	print(p[0],"的商品價格為",p[1])
-
-with open ("prdts.txt","w") as f :
-	for p in prdts :
-		f.write(p[0] + "," + p[1] + "\n")
-
+	print(p[0], "的商品價格為", p[1])
+#寫入檔案
 with open ("prdts.csv","w",encoding="utf-8") as f :
 	f.write("商品, 價格\n")
 	for p in prdts :
