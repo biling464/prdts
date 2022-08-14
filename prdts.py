3e0d7d4f621fa04c8bf83fc25d98de31e3f9ceed
Biling Xu <akaimder@gmail.com>
2022-08-14 15:38:49 +0800

    add header

--- prdts.py
+++ prdts.py
@@ -7,4 +7,14 @@ while True:
 	prdts.append([n,p])
 print(prdts)
 
+for p in prdts :
+	print(p[0],"的商品價格為",p[1])
 
+with open ("prdts.txt","w") as f :
+	for p in prdts :
+		f.write(p[0] + "," + p[1] + "\n")
+
+with open ("prdts.csv","w",encoding="utf-8") as f :
+	f.write("商品, 價格\n")
+	for p in prdts :
+		f.write(p[0] + "," + str(p[1]) + "\n")
